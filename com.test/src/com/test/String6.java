21d83d34b066e89ff8e013e8f32ce65b3562e1d4
Khaviya007 <80397018+Khaviya007@users.noreply.github.com>
2021-04-18 16:03:57 +0530

    Update String6.java

--- com.test/src/com/test/String6.java
+++ com.test/src/com/test/String6.java
@@ -1,26 +1,17 @@
 package com.test;
 public class String6 {
-	 public static boolean checkRotation(String st1, String st2) {  
-	        if (st1.length() != st2.length()) {  
-	            return false;  
-	        }  
-	        String st3 = st1 + st1;  
-	        if (st3.contains(st2))  
-	            return true;  
-	        else  
-	            return false;  
-	    }  
-	   
-	    public static void main(String[] args) {  
-	        String str1 = "XYZ";  
-	        String str2 = "YXZ";  
-	        System.out.println("Checking if a string is rotation of another");  
-	        if (checkRotation(str1, str2)) {  
-	            System.out.println(str2 + "is rotation of" + str1);  
-	        } else {  
-	            System.out.println(str2 + "is not rotation of" + str1);  
-	        }  
-	    }  
+	public static boolean Rotation(String st1, String st2) {  
+        return (st1.length() == st2.length()) &&((st1+st1).indexOf(st2)!=-1);               
+        }       
+    public static void main(String[] args) {  
+        String str1 = "XYZ";  
+        String str2 = "YXZ";          
+        if (Rotation(str1, str2)) {  
+            System.out.println("rotation");  
+        } else {  
+            System.out.println("not rotation");  
+        }  
+    }  
 }  
 
 	
